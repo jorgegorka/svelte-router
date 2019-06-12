3d9682a613d269bb9ada7ba164d0b404135371d9
Jorge Alvarez <jorge@alvareznavarro.es>
2019-06-13 00:41:19 +0200

    Uncomment code.

--- src/components/route.svelte
+++ src/components/route.svelte
@@ -5,8 +5,8 @@
 {#if currentRoute.layout}
   <svelte:component this={currentRoute.layout} currentRoute={{ component: currentRoute.component }} />
 {:else}
-  <!-- <svelte:component this={currentRoute.component} /> -->
+  <svelte:component this={currentRoute.component} />
 {/if}
 {#if currentRoute.nestedRoutes && currentRoute.nestedRoutes.length > 0}
-  <!-- <svelte:self currentRoute={currentRoute.nestedRoutes[0]} /> -->
+  <svelte:self currentRoute={currentRoute.nestedRoutes[0]} />
 {/if}
