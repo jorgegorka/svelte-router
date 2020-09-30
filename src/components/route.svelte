c01f49c6f03c885a66cac57f2cfa20611158daee
mohamed-dev-github <72163480+mohamed-dev-github@users.noreply.github.com>
2020-09-30 19:29:31 +0000

    Route lazy loading
    It would be nice if we could load route components using lazy loading

--- src/components/route.svelte
+++ src/components/route.svelte
@@ -1,12 +1,42 @@
 <script>
   export let currentRoute = {}
   export let params = {}
+  
+  let route = {};
+  
+  /*
+  * This should make possible lazy loading for routes.
+  * (ex: { layout: () => import('/path/to/my_layout.svelte', component: () => import('/path/to/my_component.svelte') )
+  * See rollup documentation to enable code splitting
+  */
+  $: async function (currentRoute) {
+    let { layout, component } = currentRoute;  
+    try { 
+      layout = layout(); // should return a promise
+    } catch {/* empty */}
+    
+    try {
+      component = component(); // should return a promise
+    } catch { /* empty */ }
+     
+    
+    [layout, component] = await Promise.all([layout, component])
+        .then(results => {
+          const [layout, component] = results;
+          return [
+            layout && layout.default ? layout.default : layout,
+            component && component.default ? component.default : component
+          ];
+        });
+    route = {...currentRoute, layout, component};
+    
+  }(currentRoute);
 </script>
 
-{#if currentRoute.layout}
-  <svelte:component this={currentRoute.layout} currentRoute={{ ...currentRoute, layout: '' }} {params} />
-{:else if currentRoute.component}
-  <svelte:component this={currentRoute.component} currentRoute={{ ...currentRoute, component: '' }} {params} />
-{:else if currentRoute.childRoute}
-  <svelte:self currentRoute={currentRoute.childRoute} {params} />
+{#if route.layout}
+  <svelte:component this={route.layout} currentRoute={{ ...route, layout: '' }} {params} />
+{:else if route.component}
+  <svelte:component this={route.component} currentRoute={{ ...route, component: '' }} {params} />
+{:else if route.childRoute}
+  <svelte:self currentRoute={route.childRoute} {params} />
 {/if}
